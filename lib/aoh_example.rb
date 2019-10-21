a28e5aa1ba3cc182dde7f336421b010bef7195be
Lucy Weltner <lucyiweltner@gmail.com>
2019-10-21 21:15:28 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,6 +14,8 @@ def assembled_aoh
 end
 
 def literal_aoh
+  infinite_jest = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  infinite_jest
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
